11877bab8eea6bc3a0573023918e4a16fe5c06e6
universalsigmateknologi <universalsigmateknologi@gmail.com>
2026-07-01 10:02:22 +0700

    mantap

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,9 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
+  content: [
+    "./templates/**/*.{html,js}",
+    "./**/templates/**/*.{html,js}",
+  ],
   theme: {
     extend: {},
   },
